5933517fe0dc727902124242ca1180a79aaabf7c
Ken-Smit <builtwithhandz@gmail.com>
2025-01-02 20:02:25 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+//Array for sight words
+const sightWoords = ["all", "am", "are", "at", "ate", "be", "black", "brown", "but", "came", "did", "do", "eat",
+    "four", "get", "good", "have", "he", "into", "like", "must", "new", "no", "now", "on", "our", "out", "please",
+    "pretty", "ran", "ride", "saw", "say", "she", "so", "soon", "that", "there", "they", "this", "that", "there", "they",
+    "this", "too", "under", "want", "was", "well", "went", "what", "white", "who", "will", "with", "yes"
+];
+
+//Array for non sight words
+const notSightWords = ["cat", "dog", "bat", "rug", "pen", "hat", "box", "bug", "sun", "log", "cup", "pig", "fox", "cow",
+    "hen", "cub", "pan", "jam", "ram", "bed", "rat", "van", "net", "bin", "mop", "lid", "gum", "web", "pod", "fan", "zip",
+    "map", "tap", "cap", "dot", "fin", "gem", "hut", "wig", "yam", "zap", "kit", "nut", "pot", "bag", "pet", "sled", "nest",
+    "flag", "frog", "kite", "sock", "bell", "crab", "ship"
+];
